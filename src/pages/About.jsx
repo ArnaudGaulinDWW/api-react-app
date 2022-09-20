9653fd6652f7ce81926f78fd0c1579bae8d342b8
ArnaudGaulinDWW <arnogaulin@gmail.com>
2022-09-20 16:01:57 +0200

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis vel ad
+        nisi quaerat id. Doloribus sunt laboriosam ab nulla quo, iste quaerat
+        temporibus, facilis suscipit, error velit harum molestiae perspiciatis.
+        Iure explicabo odit quo atque molestiae nobis, ut blanditiis labore
+        dolore qui mollitia quam quasi reprehenderit exercitationem quos
+        molestias dolorem dicta, animi quas fugiat, ducimus id modi adipisci.
+        Illo ullam fugit, placeat temporibus aliquam quo eligendi, repellendus
+        provident, aperiam totam quam ab mollitia id. Suscipit doloribus nisi
+        atque, omnis laboriosam dignissimos harum voluptas beatae pariatur
+        molestias reiciendis numquam unde deleniti distinctio perspiciatis
+        sapiente et. Molestiae repellat ab impedit dolorem dolorum?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis vel ad
+        nisi quaerat id. Doloribus sunt laboriosam ab nulla quo, iste quaerat
+        temporibus, facilis suscipit, error velit harum molestiae perspiciatis.
+        Iure explicabo odit quo atque molestiae nobis, ut blanditiis labore
+        dolore qui mollitia quam quasi reprehenderit exercitationem quos
+        molestias dolorem dicta, animi quas fugiat, ducimus id modi adipisci.
+        Illo ullam fugit, placeat temporibus aliquam quo eligendi, repellendus
+        provident, aperiam totam quam ab mollitia id. Suscipit doloribus nisi
+        atque, omnis laboriosam dignissimos harum voluptas beatae pariatur
+        molestias reiciendis numquam unde deleniti distinctio perspiciatis
+        sapiente et. Molestiae repellat ab impedit dolorem dolorum?
+      </p>
+    </div>
+  );
+};
+
+export default About;
